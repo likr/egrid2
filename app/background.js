5f913387b3ef8d32a565b40a6401410d9b482354
Yosuke ONOUE <onoue@likr-lab.com>
2016-04-05 21:48:15 +0900

    Fix scrolling for chrome app

--- app/background.js
+++ app/background.js
@@ -1,13 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    id: 'MyWindowID',
-    bounds: {
-      width: 800,
-      height: 600,
-      left: 100,
-      top: 100
-    },
-    minWidth: 800,
-    minHeight: 600
-  });
+  chrome.app.window.create('index.html');
 });
